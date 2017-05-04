abcaf3f8838914e60c1593705b55e03c1e93835e
Petar Petrov <primas.petrov@gmail.com>
2017-05-04 16:31:34 +0300

    Updated Packages and Added Base Model

---
+++ UCRS.Data/BaseModel.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace UCRS.Data
+{
+    /// <summary>
+    /// Base class for Data Models
+    /// </summary>
+    public abstract class BaseModel
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="BaseModel"/> class.
+        /// </summary>
+        protected BaseModel()
+        {
+            this.Id = Guid.NewGuid();
+            this.CreatedOn = DateTime.Now;
+        }
+
+        /// <summary>
+        /// Gets or sets the identifier.
+        /// </summary>
+        /// <value>
+        /// The identifier.
+        /// </value>
+        [Key]
+        public Guid Id { get; set; }
+
+        /// <summary>
+        /// Gets or sets the created on.
+        /// </summary>
+        /// <value>
+        /// The created on.
+        /// </value>
+        public DateTime CreatedOn { get; set; }
+
+        /// <summary>
+        /// Gets or sets the modified on.
+        /// </summary>
+        /// <value>
+        /// The modified on.
+        /// </value>
+        public DateTime? ModifiedOn { get; set; }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether this instance is deleted.
+        /// </summary>
+        /// <value>
+        /// <c>true</c> if this instance is deleted; otherwise, <c>false</c>.
+        /// </value>
+        [Index]
+        public bool IsDeleted { get; set; }
+
+        /// <summary>
+        /// Gets or sets the deleted on.
+        /// </summary>
+        /// <value>
+        /// The deleted on.
+        /// </value>
+        public DateTime? DeletedOn { get; set; }
+    }
+}
