c8857d89d8b62f5677451b60902adbbbb6f9e8d8
Petar Petrov <primas.petrov@gmail.com>
2017-05-04 18:10:27 +0300

    Using update

--- UCRS.Web/Controllers/AccountController.cs
+++ UCRS.Web/Controllers/AccountController.cs
@@ -1,10 +1,11 @@
-﻿using Microsoft.AspNet.Identity;
-using Microsoft.AspNet.Identity.Owin;
-using Microsoft.Owin.Security;
-using System.Linq;
+﻿using System.Linq;
 using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
+
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.Owin;
+using Microsoft.Owin.Security;
 
 using UCRS.Data.Models;
 using UCRS.Web.Models;
