79ea2edd69d127493de4fa9faee860016487b3b8
Petar Petrov <primas.petrov@gmail.com>
2017-05-05 17:28:29 +0300

    Added Ninject to the solution

--- UCRS.Web/Controllers/HomeController.cs
+++ UCRS.Web/Controllers/HomeController.cs
@@ -1,10 +1,19 @@
 ﻿using System.Web.Mvc;
 
+using UCRS.Common.Contracts;
+using UCRS.Data;
+using UCRS.Services;
+
 namespace UCRS.Web.Controllers
 {
     public class HomeController : Controller
     {
+        private IStudentService _studentService = null;
 
+        public HomeController()
+        {
+            this._studentService = new StudentService(new UniversitySystemDbContext());
+        }
 
         public ActionResult Index()
         {
